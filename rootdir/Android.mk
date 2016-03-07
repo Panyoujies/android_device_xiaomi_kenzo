e4e288d958597785c56d480761ffb5053d54716b
TheStrix <parthbhatia98@gmail.com>
2016-03-07 13:06:33 +0530

    kenzo: Add init.qcom.post_boot.sh
    Change-Id: I9d4015445ba27633d840aa050862bcf9e5c20bac

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -42,6 +42,7 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+<<<<<<< HEAD
 LOCAL_MODULE		:= init.qcom.early_boot.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
@@ -79,6 +80,13 @@ LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.usb.sh
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.post_boot.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
